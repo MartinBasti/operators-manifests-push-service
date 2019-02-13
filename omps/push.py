dbbd864c701672e3b1b0c9e1a875ba5139a3138f
Martin Bašti <mbasti@redhat.com>
2019-02-13 17:07:22 +0100

    Add support for uploading and extracting zip files
    This is first requirement to be able handle operators manifests uploaded
    in zip files.
    
    * OSBS-6688
    
    Signed-off-by: Martin Bašti <mbasti@redhat.com>

--- omps/push.py
+++ omps/push.py
@@ -2,10 +2,80 @@
 # Copyright (C) 2019 Red Hat, Inc
 # see the LICENSE file for license
 #
+import logging
+import os
+from tempfile import NamedTemporaryFile, TemporaryDirectory
+import zipfile
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, current_app, request
 
+from .constants import (
+    ALLOWED_EXTENSIONS,
+    DEFAULT_ZIPFILE_MAX_UNCOMPRESSED_SIZE,
+)
+from .errors import OMPSUploadedFileError, OMPSExpectedFileError
+
+logger = logging.getLogger(__name__)
 BLUEPRINT = Blueprint('push', __name__)
+
+
+def validate_allowed_extension(filename):
+    """Check file extension"""
+    if '.' in filename:
+        extension = filename.rsplit('.', 1)[1].lower()
+        if extension not in ALLOWED_EXTENSIONS:
+            raise OMPSUploadedFileError(
+                'Uploaded file extension "{}" is not allowed'.format(extension)
+            )
+
+
+def extract_zip_file(req, target_dir,
+                     max_uncompressed_size=DEFAULT_ZIPFILE_MAX_UNCOMPRESSED_SIZE):
+    """Function store uploaded file in target_directory
+
+    :param req: Flask request object
+    :param target_dir: directory where file will be stored
+    :param max_uncompressed_size: size in Bytes how big data can be accepted
+        after uncompressing
+    """
+    assert req.method == 'POST'
+    if 'file' not in req.files:
+        raise OMPSExpectedFileError('No "file" in upload')
+    file = req.files['file']
+    if not file.filename:
+        raise OMPSExpectedFileError('No selected "file" in upload')
+
+    validate_allowed_extension(file.filename)
+
+    with NamedTemporaryFile('w', suffix='.zip', dir=target_dir) as tmpf:
+        file.save(tmpf.name)
+        archive = zipfile.ZipFile(tmpf.name)
+        if logger.isEnabledFor(logging.DEBUG):
+            # log content of zipfile
+            logger.debug(
+                'Content of uploaded zip archive "%s":\n%s',
+                file.filename, '\n'.join(
+                    "name={zi.filename}, compress_size={zi.compress_size}, "
+                    "file_size={zi.file_size}".format(zi=zipinfo)
+                    for zipinfo in archive.filelist
+                )
+            )
+
+        uncompressed_size = sum(zi.file_size for zi in archive.filelist)
+        if uncompressed_size > max_uncompressed_size:
+            raise OMPSUploadedFileError(
+                "Uncompressed archive may reach max size limit "
+                "({}B>{}B)".format(
+                    uncompressed_size, max_uncompressed_size
+                ))
+
+        bad_file = archive.testzip()
+        if bad_file is not None:
+            raise OMPSUploadedFileError(
+                "CRC check failed for file {} in archive".format(bad_file)
+            )
+        archive.extractall(target_dir)
+        archive.close()
 
 
 @BLUEPRINT.route("/<organization>/<repo>/zipfile", methods=('POST',))
@@ -21,6 +91,15 @@ def push_zipfile(organization, repo):
         'repo': repo,
         'msg': 'Not Implemented. Testing only'
     }
+
+    with TemporaryDirectory() as tmpdir:
+        max_size = current_app.config.get(
+            'ZIPFILE_MAX_UNCOMPRESSED_SIZE',
+            DEFAULT_ZIPFILE_MAX_UNCOMPRESSED_SIZE
+        )
+        extract_zip_file(request, tmpdir,
+                         max_uncompressed_size=max_size)
+        data['extracted_files'] = os.listdir(tmpdir)
     resp = jsonify(data)
     resp.status_code = 200
     return resp
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -44,3 +44,45 @@ def test_log_format():
 
     conf = Config(ConfClass)
     assert conf.log_format == expected
+
+
+def test_max_content_length():
+    """Test of setting max_content_length"""
+    expected = 10
+
+    class ConfClass(DefaultConfig):
+        MAX_CONTENT_LENGTH = expected
+
+    conf = Config(ConfClass)
+    assert conf.max_content_length == expected
+
+
+def test_max_content_length_invalid():
+    """Test of setting invalid max_content_length"""
+
+    class ConfClass(DefaultConfig):
+        MAX_CONTENT_LENGTH = -10
+
+    with pytest.raises(ValueError):
+        Config(ConfClass)
+
+
+def test_zipfile_max_uncompressed_size():
+    """Test of setting zipfile_max_uncompressed_size"""
+    expected = 10
+
+    class ConfClass(DefaultConfig):
+        ZIPFILE_MAX_UNCOMPRESSED_SIZE = expected
+
+    conf = Config(ConfClass)
+    assert conf.zipfile_max_uncompressed_size == expected
+
+
+def test_zipfile_max_uncompressed_size_invalid():
+    """Test of setting invalid zipfile_max_uncompressed_size"""
+
+    class ConfClass(DefaultConfig):
+        ZIPFILE_MAX_UNCOMPRESSED_SIZE = -10
+
+    with pytest.raises(ValueError):
+        Config(ConfClass)
